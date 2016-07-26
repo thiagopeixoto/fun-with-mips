9926dca3b18523799c8776d857b9697c411cd48a
Thiago Peixoto <thiagopeixoto@linux.com>
2016-07-26 02:09:39 +0000

    Modication on file e.s.

--- e.s
+++ e.s
@@ -1,17 +1,17 @@
 	.globl __start
 	.data
-number: .space 32
+buffer: .space 32
 	.text
 
 __start:
 	li	$v0, 4003
 	li   	$a0, 0
-	la      $a1, number
+	la      $a1, buffer
 	li     	$a2, 32
 	syscall
 	#li	$v0, 4004
 	#li	$a0, 1
-	#la 	$a1, number
+	#la 	$a1, buffer
 	#la	$a2, 32
 	#syscall
 	li   	$v0, 4001
